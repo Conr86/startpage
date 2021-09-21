d589efa7b03fe694adbff7a6e4429360698d04c5
Connor Kneebone <Connor.Kneebone@fusion-centre.com>
2021-09-21 15:07:14 +1000

    Transparency, links as data, etc.

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,9 @@
 	export let name;
 	export let apiKey;
 	export let location;
-
-	console.log(apiKey);
-
+	export let columns;
+	export let doubleSpacedList;
+	
 	let d = new Date();
 	$: hours = format_two_digits(d.getHours() % 12 === 0 ? 12 : d.getHours() % 12);
 	$: minutes = format_two_digits(d.getMinutes());
@@ -26,7 +26,7 @@
 
 	function updateWeather() {
 		if (apiKey && location) {
-			fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`)
+			fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`)
 				.then(res => res.json())
 				.then((out) => {
 					weather = out;
@@ -64,147 +64,11 @@
 		return n < 10 ? '0' + n : n;
 	}
 </script>
-
-<style>
-	main {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		height: inherit;
-	}
-	#box {
-		display: flex;
-		width: 70rem;
-		height: 32rem;
-		margin: auto;
-		background: var(--bg2-color);
-		border-radius: 1rem;
-		box-shadow: 0 0.5rem 1rem #00000050;
-		animation: appear 0.4s ease-out forwards;
-	}
-	#image-container {
-		display: inline-block;
-		position: relative;
-		height: inherit;
-		margin: 0;
-	}
-	#image {
-		height: inherit;
-		border-radius: 1rem 0 0rem 1rem;
-		filter: brightness(0.8);
-	}
-	#time {
-		position: absolute;
-		width: 3.5rem;
-		top: 50%;
-		left: 50%;
-    	transform: translate(-50%, -50%) scale(0%);
-		padding: 1rem;
-		overflow-wrap: break-word;
-		text-align: center;
-		font-size: 3.5rem;
-		background: #00000050;
-		color: var(--text-light-color);
-		border-radius: 1rem;
-		mix-blend-mode: overlay;
-		animation: time-appear 0.4s ease-out 0.2s forwards;
-	}
-	#inner-box {
-		width: 100%;
-		padding: 4rem;
-	}
-	h1 {
-		margin: 0;
-		font-weight: inherit;
-		font-size: 2rem;
-        opacity: 0;
-		animation: text-appear 0.4s ease-out 0.2s forwards;
-	}
-	h2 {
-		margin: 0;
-		font-weight: inherit;
-		font-size: 1.5rem;
-        opacity: 0;
-		animation: text-appear 0.4s ease-out 0.25s forwards;
-	}
-	#links {
-		display: flex;
-		justify-content: space-between;
-		margin: 0;
-	}
-	.link-column {
-		width: 7.5rem;
-		opacity: 0;
-	}
-	#link-column-1 {
-		animation: text-appear 0.4s ease-out 0.3s forwards;
-	}
-	#link-column-2 {
-		animation: text-appear 0.4s ease-out 0.35s forwards;
-	}
-	#link-column-3 {
-		animation: text-appear 0.4s ease-out 0.4s forwards;
-	}
-	a {
-		display: inline-block;
-		text-decoration: none;
-    	color: var(--text-dark-color);
-		transition: 0.3s ease;
-	}
-	.arrow {
-    	display: inline-block;
-		transform: translate(-22px);
-		opacity: 0;
-		transition: 0.3s ease;
-	}
-	.text {
-    	display: inline-block;
-		transform: translate(-22px);
-		transition: 0.3s ease;
-	}
-	a:hover {
-    	color: var(--text-color);
-	}
-	a:hover .arrow {
-		transform: none;
-		opacity: 1;
-	}
-	a:hover .text {
-		transform: none;
-	}
-	a:active:hover {
-		transform: scale(0.9);
-	}
-	br {
-		line-height: 2rem;
-	}
-	#weather-container {
-		position: absolute;
-		top: 0;
-		right: 0;
-		margin: 4rem 5rem;
-		opacity: 0;
-		animation: weather-appear 0.4s ease-out 0.4s forwards;
-	}
-	#temperature {
-		display: inline-block;
-		margin: 0.3rem 0.2rem 0 0;
-		font-size: 1.5rem;
-		vertical-align: top;
-	}
-	#version {
-		position: absolute;
-		margin: 1rem;
-		bottom: 0;
-		right: 0;
-		color: var(--text-dark-color);
-	}
-</style>
-
+<div class="bg-image"></div>
 <main>
 	<div id="box">
 		<div id="image-container">
-			<img id="image" src="gifs/bg-1.gif" alt="">
+			
 			<div id="time">{hours}-{minutes}</div>
 		</div>
 		<div id="inner-box">
@@ -217,57 +81,20 @@
 			<h2 id="header-2">Today is {date}.</h2>
 			<br><br>
 			<div id="links">
-				<div class="link-column" id="link-column-1">
-					<a href="https://mail.google.com">
-						<span class="arrow">></span> <span class="text">gmail</span>
-					</a>
-					<br><br>
-					<a href="https://calendar.google.com">
-						<span class="arrow">></span> <span class="text">calendar</span>
-					</a>
-					<br><br>
-					<a href="https://drive.google.com">
-						<span class="arrow">></span> <span class="text">drive</span>
-					</a>
-					<br><br>
-					<a href="https://docs.google.com">
-						<span class="arrow">></span> <span class="text">docs</span>
-					</a>
-				</div>
-				<div class="link-column" id="link-column-2">
-					<a href="https://github.com">
-						<span class="arrow">></span> <span class="text">github</span>
-					</a>
-					<br><br>
-					<a href="https://stackoverflow.com">
-						<span class="arrow">></span> <span class="text">stack</span>
-					</a>
-					<br><br>
-					<a href="https://dashboard.heroku.com">
-						<span class="arrow">></span> <span class="text">heroku</span>
-					</a>
-					<br><br>
-					<a href="https://wa-bsd405-psv.edupoint.com/Home_PXP2.aspx">
-						<span class="arrow">></span> <span class="text">synergy</span>
-					</a>
-				</div>
-				<div class="link-column" id="link-column-3">
-					<a href="https://youtube.com">
-						<span class="arrow">></span> <span class="text">youtube</span>
-					</a>
-					<br><br>
-					<a href="https://twitch.tv">
-						<span class="arrow">></span> <span class="text">twitch</span>
-					</a>
-					<br><br>
-					<a href="https://reddit.com">
-						<span class="arrow">></span> <span class="text">reddit</span>
-					</a>
-					<br><br>
-					<a href="http://monkeytype.com">
-						<span class="arrow">></span> <span class="text">type</span>
-					</a>
-				</div>
+				{#each columns as column, i}
+					<div class="link-column" id="link-column-{i+1}">
+						{#each column as {name, url}}
+							<a href="{url}" target="_top">
+								<span class="arrow">></span> <span class="text">{name}</span>
+							</a>
+							{#if doubleSpacedList}
+								<br>
+							{/if}
+							<br>
+						{/each}
+					</div>
+				{/each}
+				
 			</div>
 			{#if weather}
 				<div id="weather-container">
@@ -277,5 +104,5 @@
 			{/if}
 		</div>
 	</div>
-	<div id="version">v{version}</div>
 </main>
+<div id="version">v{version}</div>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,74 @@ const app = new App({
 	target: document.body,
 	props: {
 		// CHANGE THESE VARIABLES
-		name: '',
-		location: '',
-		apiKey: ''
+		name: 'Connor',
+		location: 'Sydney, AU',
+		apiKey: 'your-key-here',
+		doubleSpacedList: true,
+		columns: [
+			[{
+					"name": "gmail",
+					"url": "https://mail.google.com"
+				},
+				{
+					"name": "calendar",
+					"url": "https://calendar.google.com"
+				},
+				{
+					"name": "drive",
+					"url": "https://drive.google.com"
+				},
+				{
+					"name": "photos",
+					"url": "https://photos.google.com"
+				}
+			],
+			[
+				{
+					"name": "abc",
+					"url": "https://www.abc.net.au/news/"
+				},
+				{
+					"name": "new york times",
+					"url": "https://www.nytimes.com/"
+				},
+				{
+					"name": "financial review",
+					"url": "https://www.afr.com/"
+				},
+				{
+					"name": "the economist",
+					"url": "https://www.economist.com/"
+				}
+			],
+			[
+				{
+					"name": "twitter",
+					"url": "https://twitter.com/"
+				},
+				{
+					"name": "reddit",
+					"url": "https://www.nytimes.com/"
+				},
+				{
+					"name": "facebook",
+					"url": "https://www.afr.com/"
+				}
+			],
+			[{
+					"name": "github",
+					"url": "https://github.com/"
+				},
+				{
+					"name": "azure",
+					"url": "https://portal.azure.com/#home"
+				},
+				{
+					"name": "devops",
+					"url": "https://dev.azure.com/"
+				}
+			]
+		]
 	}
 });
 
